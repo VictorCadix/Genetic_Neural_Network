0f2f3932cd657e54aee6655249f5b9d21f227f64
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2017-08-31 20:39:57 +0200

    Correction in the rows<->colons
    Weights vector (form a neuron) is represented as the colons in the genes matrix.

--- Neural Net 2/Red.cpp
+++ Neural Net 2/Red.cpp
@@ -153,9 +153,9 @@ void Red::forwardProp()
 
 void Red::genes2weights() {
 	for (int i = 0; i < this->nLayers - 1; i++) {
-		for (int j = 0; j < this->structure[i + 2]; j++) { 
-			for (int k = 0; k < this->structure[i + 1]+1; k++) {//[]+1 for bias
-				layers[i + 1][j].pesos[k] = genes[i][j][k];
+		for (int j = 0; j < this->structure[i + 1]+1; j++) {//[]+1 for bias 
+			for (int k = 0; k < this->structure[i + 2]; k++) {
+				layers[i + 1][k].pesos[j] = genes[i][j][k];
 			}
 		}
 	}
