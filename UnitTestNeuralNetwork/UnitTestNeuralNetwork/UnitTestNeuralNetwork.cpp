f0b471b5652232bfaa2335fd0769e341cbc862fc
Raul6469 <24607388+Raul6469@users.noreply.github.com>
2017-08-31 17:46:09 +0200

    Change test architecture

--- UnitTestNeuralNetwork/UnitTestNeuralNetwork/UnitTestNeuralNetwork.cpp
+++ UnitTestNeuralNetwork/UnitTestNeuralNetwork/UnitTestNeuralNetwork.cpp
@@ -10,7 +10,7 @@
 #include "../../Neural Net 2/Neurona.h"
 #include "../../Neural Net 2/Red.h"
 
-TEST_CASE("Creation d'un objet Neurona classique bien forme")
+TEST_CASE("Creation d'un objet Neurona")
 {
 	SECTION("Neurone simple")
 	{
@@ -21,12 +21,12 @@ TEST_CASE("Creation d'un objet Neurona classique bien forme")
 		REQUIRE(neuroneTest.posCapa == 1);
 		REQUIRE(neuroneTest.neuronasCapaAnterior == 1);
 
-		for (int p = 0; p<neuroneTest.neuronasCapaAnterior; p++) {
+		for (int p = 0; p < neuroneTest.neuronasCapaAnterior; p++) {
 			REQUIRE(neuroneTest.pesos[p] <= 10);
 			REQUIRE(neuroneTest.pesos[p] >= -10);
 		}
 	}
-	
+
 	SECTION("Neurone plus complexe")
 	{
 		Neurona neuroneTest = Neurona(3, 3, 3);
@@ -36,47 +36,48 @@ TEST_CASE("Creation d'un objet Neurona classique bien forme")
 		REQUIRE(neuroneTest.posCapa == 3);
 		REQUIRE(neuroneTest.neuronasCapaAnterior == 3);
 
-		for (int p = 0; p<neuroneTest.neuronasCapaAnterior; p++) {
+		for (int p = 0; p < neuroneTest.neuronasCapaAnterior; p++) {
 			REQUIRE(neuroneTest.pesos[p] <= 10);
 			REQUIRE(neuroneTest.pesos[p] >= -10);
 		}
 	}
-	
-}
 
-TEST_CASE("Test creation neurone par defaut")
-{
-	Neurona neuroneTest = Neurona();
 
-	REQUIRE(neuroneTest.getValor() == 0);
-	REQUIRE(neuroneTest.posNeurona == 0);
-	REQUIRE(neuroneTest.posCapa == 0);
-	REQUIRE(neuroneTest.neuronasCapaAnterior == 0);
-	REQUIRE(neuroneTest.pesos == NULL);
-}
 
-TEST_CASE("Creation bias neuron")
-{
-	Neurona testNeuron = Neurona(2, 2, -1);
+	SECTION("Test creation neurone par defaut")
+	{
+		Neurona neuroneTest = Neurona();
 
-	REQUIRE(testNeuron.getValor() == 1);
-	REQUIRE(testNeuron.posNeurona == 2);
-	REQUIRE(testNeuron.posCapa == 2);
-	REQUIRE(testNeuron.neuronasCapaAnterior == 0);
-	for (int p = 0; p<testNeuron.neuronasCapaAnterior; p++) {
-		REQUIRE(testNeuron.pesos[p] <= 10);
-		REQUIRE(testNeuron.pesos[p] >= -10);
+		REQUIRE(neuroneTest.getValor() == 0);
+		REQUIRE(neuroneTest.posNeurona == 0);
+		REQUIRE(neuroneTest.posCapa == 0);
+		REQUIRE(neuroneTest.neuronasCapaAnterior == 0);
+		REQUIRE(neuroneTest.pesos == NULL);
 	}
-}
 
-TEST_CASE("Creation input neuron")
-// A COMPLETER
-{
-	Neurona testNeuron = Neurona(0, 2, 0);
+	SECTION("Creation bias neuron")
+	{
+		Neurona testNeuron = Neurona(2, 2, -1);
 
-	REQUIRE(testNeuron.getValor() == 0);
-	REQUIRE(testNeuron.posNeurona == 2);
-	REQUIRE(testNeuron.posCapa == 0);
-	REQUIRE(testNeuron.neuronasCapaAnterior == 0);
-	REQUIRE(testNeuron.pesos == NULL);
+		REQUIRE(testNeuron.getValor() == 1);
+		REQUIRE(testNeuron.posNeurona == 2);
+		REQUIRE(testNeuron.posCapa == 2);
+		REQUIRE(testNeuron.neuronasCapaAnterior == 0);
+		for (int p = 0; p < testNeuron.neuronasCapaAnterior; p++) {
+			REQUIRE(testNeuron.pesos[p] <= 10);
+			REQUIRE(testNeuron.pesos[p] >= -10);
+		}
+	}
+
+	SECTION("Creation input neuron")
+	// A COMPLETER
+	{
+		Neurona testNeuron = Neurona(0, 2, 0);
+
+		REQUIRE(testNeuron.getValor() == 0);
+		REQUIRE(testNeuron.posNeurona == 2);
+		REQUIRE(testNeuron.posCapa == 0);
+		REQUIRE(testNeuron.neuronasCapaAnterior == 0);
+		REQUIRE(testNeuron.pesos == NULL);
+	}
 }
