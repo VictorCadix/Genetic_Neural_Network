e316c99b16c7ff3ee1bd37c05a4c43404ab73b07
Raul6469 <24607388+Raul6469@users.noreply.github.com>
2017-08-31 17:32:30 +0200

    Use test sections + test default neuron

--- UnitTestNeuralNetwork/UnitTestNeuralNetwork/UnitTestNeuralNetwork.cpp
+++ UnitTestNeuralNetwork/UnitTestNeuralNetwork/UnitTestNeuralNetwork.cpp
@@ -12,25 +12,43 @@
 
 TEST_CASE("Creation d'un objet Neurona classique bien forme")
 {
-	Neurona neuroneTest = Neurona(1, 1, 1);
-	REQUIRE(neuroneTest.getValor() == 0);
-	REQUIRE(neuroneTest.posNeurona == 1);
-	REQUIRE(neuroneTest.posCapa == 1);
-	REQUIRE(neuroneTest.neuronasCapaAnterior == 1);
+	SECTION("Neurone simple")
+	{
+		Neurona neuroneTest = Neurona(1, 1, 1);
+		REQUIRE(neuroneTest.getValor() == 0);
+		REQUIRE(neuroneTest.posNeurona == 1);
+		REQUIRE(neuroneTest.posCapa == 1);
+		REQUIRE(neuroneTest.neuronasCapaAnterior == 1);
+
+		for (int p = 0; p<neuroneTest.neuronasCapaAnterior; p++) {
+			REQUIRE(neuroneTest.pesos[p] <= 10);
+			REQUIRE(neuroneTest.pesos[p] >= -10);
+		}
+	}
 	
-	for (int p = 0; p<neuroneTest.neuronasCapaAnterior; p++) {
-		REQUIRE(neuroneTest.pesos[p] <= 10);
-		REQUIRE(neuroneTest.pesos[p] >= -10);
-	}
+	SECTION("Neurone plus complexe")
+	{
+		Neurona neuroneTest = Neurona(3, 3, 3);
+		REQUIRE(neuroneTest.getValor() == 0);
+		REQUIRE(neuroneTest.posNeurona == 3);
+		REQUIRE(neuroneTest.posCapa == 3);
+		REQUIRE(neuroneTest.neuronasCapaAnterior == 3);
 
-	neuroneTest = Neurona(3, 3, 3);
+		for (int p = 0; p<neuroneTest.neuronasCapaAnterior; p++) {
+			REQUIRE(neuroneTest.pesos[p] <= 10);
+			REQUIRE(neuroneTest.pesos[p] >= -10);
+		}
+	}
+	
+}
+
+TEST_CASE("Test creation neurone par defaut")
+{
+	Neurona neuroneTest = Neurona();
+
 	REQUIRE(neuroneTest.getValor() == 0);
-	REQUIRE(neuroneTest.posNeurona == 3);
-	REQUIRE(neuroneTest.posCapa == 3);
-	REQUIRE(neuroneTest.neuronasCapaAnterior == 3);
-
-	for (int p = 0; p<neuroneTest.neuronasCapaAnterior; p++) {
-		REQUIRE(neuroneTest.pesos[p] <= 10);
-		REQUIRE(neuroneTest.pesos[p] >= -10);
-	}
+	REQUIRE(neuroneTest.posNeurona == 0);
+	REQUIRE(neuroneTest.posCapa == 0);
+	REQUIRE(neuroneTest.neuronasCapaAnterior == 0);
+	REQUIRE(neuroneTest.pesos == NULL);
 }
