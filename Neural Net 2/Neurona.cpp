b98cb7d53416c86f0799c39b1785ec5bc7fa72bf
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2017-08-31 21:37:22 +0200

    Weights in bias neurons solved

--- Neural Net 2/Neurona.cpp
+++ Neural Net 2/Neurona.cpp
@@ -16,25 +16,28 @@ Neurona::Neurona(int capa, int neurona,int nNeuronasCapaAnterior){
 	this->posNeurona = neurona;
 	this->neuronasCapaAnterior = nNeuronasCapaAnterior;
 
+	//Bias neuron
 	if (this->neuronasCapaAnterior < 0) { // -1 for bias neuron
 		this->valor = 1;
+		this->pesos = NULL;
 		this->neuronasCapaAnterior = 0;
 		cout << "Bias created: layer " << this->posCapa << ", pos " << this->posNeurona << endl;
 	}
 
+	//Input neuron
 	else if (this->posCapa == 0) {
+		this->valor = 0;
+		this->pesos = NULL;
 		cout << "Input neuron created: layer " << this->posCapa << ", pos " << this->posNeurona << endl;
 	}
+
+	//Normal neuron
 	else
 	{
 		this->valor = 0;
+		this->pesos = new double[neuronasCapaAnterior];
 		cout << "Neuron created: layer " << this->posCapa << ", pos " << this->posNeurona << endl;
 	}
-
-	if (this->posCapa == 0)
-		this->pesos = NULL;
-	else
-		this->pesos =  new double [neuronasCapaAnterior];
 
 	//Inicializa los pesos
 	for (int p=0; p<this->neuronasCapaAnterior; p++){
