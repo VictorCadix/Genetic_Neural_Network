bfe134ffe1792875ed2ff81ab9802dde19b3c930
Raul6469 <24607388+Raul6469@users.noreply.github.com>
2017-09-01 11:49:12 +0200

    Use CHECK instead of REQUIRE

--- UnitTestNeuralNetwork/UnitTestNeuralNetwork/UnitTestNeuralNetwork.cpp
+++ UnitTestNeuralNetwork/UnitTestNeuralNetwork/UnitTestNeuralNetwork.cpp
@@ -16,14 +16,14 @@ TEST_CASE("Creation d'un objet Neurona")
 	{
 		Neurona neuroneTest = Neurona(1, 1, 1);
 
-		REQUIRE(neuroneTest.getValor() == 0);
-		REQUIRE(neuroneTest.posNeurona == 1);
-		REQUIRE(neuroneTest.posCapa == 1);
-		REQUIRE(neuroneTest.neuronasCapaAnterior == 1);
+		CHECK(neuroneTest.getValor() == 0);
+		CHECK(neuroneTest.posNeurona == 1);
+		CHECK(neuroneTest.posCapa == 1);
+		CHECK(neuroneTest.neuronasCapaAnterior == 1);
 
 		for (int p = 0; p < neuroneTest.neuronasCapaAnterior; p++) {
-			REQUIRE(neuroneTest.pesos[p] <= 10);
-			REQUIRE(neuroneTest.pesos[p] >= -10);
+			CHECK(neuroneTest.pesos[p] <= 10);
+			CHECK(neuroneTest.pesos[p] >= -10);
 		}
 	}
 
@@ -31,14 +31,14 @@ TEST_CASE("Creation d'un objet Neurona")
 	{
 		Neurona neuroneTest = Neurona(3, 3, 3);
 
-		REQUIRE(neuroneTest.getValor() == 0);
-		REQUIRE(neuroneTest.posNeurona == 3);
-		REQUIRE(neuroneTest.posCapa == 3);
-		REQUIRE(neuroneTest.neuronasCapaAnterior == 3);
+		CHECK(neuroneTest.getValor() == 0);
+		CHECK(neuroneTest.posNeurona == 3);
+		CHECK(neuroneTest.posCapa == 3);
+		CHECK(neuroneTest.neuronasCapaAnterior == 3);
 
 		for (int p = 0; p < neuroneTest.neuronasCapaAnterior; p++) {
-			REQUIRE(neuroneTest.pesos[p] <= 10);
-			REQUIRE(neuroneTest.pesos[p] >= -10);
+			CHECK(neuroneTest.pesos[p] <= 10);
+			CHECK(neuroneTest.pesos[p] >= -10);
 		}
 	}
 
@@ -46,24 +46,24 @@ TEST_CASE("Creation d'un objet Neurona")
 	{
 		Neurona neuroneTest = Neurona();
 
-		REQUIRE(neuroneTest.getValor() == 0);
-		REQUIRE(neuroneTest.posNeurona == 0);
-		REQUIRE(neuroneTest.posCapa == 0);
-		REQUIRE(neuroneTest.neuronasCapaAnterior == 0);
-		REQUIRE(neuroneTest.pesos == NULL);
+		CHECK(neuroneTest.getValor() == 0);
+		CHECK(neuroneTest.posNeurona == 0);
+		CHECK(neuroneTest.posCapa == 0);
+		CHECK(neuroneTest.neuronasCapaAnterior == 0);
+		CHECK(neuroneTest.pesos == NULL);
 	}
 
 	SECTION("Creation bias neuron")
 	{
 		Neurona testNeuron = Neurona(2, 2, -1);
 
-		REQUIRE(testNeuron.getValor() == 1);
-		REQUIRE(testNeuron.posNeurona == 2);
-		REQUIRE(testNeuron.posCapa == 2);
-		REQUIRE(testNeuron.neuronasCapaAnterior == 0);
+		CHECK(testNeuron.getValor() == 1);
+		CHECK(testNeuron.posNeurona == 2);
+		CHECK(testNeuron.posCapa == 2);
+		CHECK(testNeuron.neuronasCapaAnterior == 0);
 		for (int p = 0; p < testNeuron.neuronasCapaAnterior; p++) {
-			REQUIRE(testNeuron.pesos[p] <= 10);
-			REQUIRE(testNeuron.pesos[p] >= -10);
+			CHECK(testNeuron.pesos[p] <= 10);
+			CHECK(testNeuron.pesos[p] >= -10);
 		}
 	}
 
@@ -72,11 +72,11 @@ TEST_CASE("Creation d'un objet Neurona")
 	{
 		Neurona testNeuron = Neurona(0, 2, 0);
 
-		REQUIRE(testNeuron.getValor() == 0);
-		REQUIRE(testNeuron.posNeurona == 2);
-		REQUIRE(testNeuron.posCapa == 0);
-		REQUIRE(testNeuron.neuronasCapaAnterior == 0);
-		REQUIRE(testNeuron.pesos == NULL);
+		CHECK(testNeuron.getValor() == 0);
+		CHECK(testNeuron.posNeurona == 2);
+		CHECK(testNeuron.posCapa == 0);
+		CHECK(testNeuron.neuronasCapaAnterior == 0);
+		CHECK(testNeuron.pesos == NULL);
 	}
 }
 
@@ -87,7 +87,7 @@ TEST_CASE("Fonction d'activation")
 		Neurona test = Neurona(1, 1, 1);
 		test.setValor(3);
 		test.activate_sigmoid();
-		REQUIRE(abs(test.getValor() - 0.95) < 0.01);
+		CHECK(abs(test.getValor() - 0.95) < 0.01);
 	}
 	
 	SECTION("Fonction tanh")
@@ -95,7 +95,7 @@ TEST_CASE("Fonction d'activation")
 		Neurona test = Neurona(1, 1, 1);
 		test.setValor(3);
 		test.activate_tanh();
-		REQUIRE(abs(test.getValor() - (-0.99)) < 0.01);
+		CHECK(abs(test.getValor() - (-0.99)) < 0.01);
 	}
 }
 
@@ -106,28 +106,28 @@ TEST_CASE("Constructeur de Red")
 
 	SECTION("Attribut structure correct")
 	{
-		REQUIRE(structure[0] == 3);
-		REQUIRE(structure[1] == 2);
-		REQUIRE(structure[2] == 3);
-		REQUIRE(structure[3] == 1);
+		CHECK(structure[0] == 3);
+		CHECK(structure[1] == 2);
+		CHECK(structure[2] == 3);
+		CHECK(structure[3] == 1);
 	}
 
 	SECTION("Attrbiut nombre de couches")
 	{
-		REQUIRE(red.nLayers == 3);
+		CHECK(red.nLayers == 3);
 	}
 
 	SECTION("Nombre de couches crees")
 	{
-		REQUIRE(red.layers.size() == 3);
+		CHECK(red.layers.size() == 3);
 	}
 
 	SECTION("Nombre de neurones dans chaque couche")
 	{
 		//One more neuron (the bias) on all layers but the last one
-		REQUIRE(red.layers[0].size() == 3);
-		REQUIRE(red.layers[1].size() == 4);
-		REQUIRE(red.layers[2].size() == 1);
+		CHECK(red.layers[0].size() == 3);
+		CHECK(red.layers[1].size() == 4);
+		CHECK(red.layers[2].size() == 1);
 	}
 
 	SECTION("Premiere couche d'input neurons")
@@ -135,10 +135,10 @@ TEST_CASE("Constructeur de Red")
 		Layer couche = red.layers[0];
 		for (int i = 0; i < couche.size(); i++)
 		{
-			REQUIRE(couche[i].neuronasCapaAnterior == 0);
-			REQUIRE(couche[i].posNeurona == i);
-			REQUIRE(couche[i].posCapa == 0);
-			REQUIRE(couche[i].pesos == NULL);
+			CHECK(couche[i].neuronasCapaAnterior == 0);
+			CHECK(couche[i].posNeurona == i);
+			CHECK(couche[i].posCapa == 0);
+			CHECK(couche[i].pesos == NULL);
 		}
 	}
 
@@ -148,10 +148,10 @@ TEST_CASE("Constructeur de Red")
 		{
 			Layer couche = red.layers[i];
 			Neurona neuron = couche[couche.size()-1];
-			REQUIRE(neuron.getValor() == 1);
-			REQUIRE(neuron.posCapa == i);
-			REQUIRE(neuron.neuronasCapaAnterior == 0);
-			//REQUIRE(neuron.pesos == NULL);
+			CHECK(neuron.getValor() == 1);
+			CHECK(neuron.posCapa == i);
+			CHECK(neuron.neuronasCapaAnterior == 0);
+			//CHECK(neuron.pesos == NULL);
 		}
 	}
 
@@ -170,8 +170,8 @@ TEST_CASE("Reglage des inputs du reseau")
 
 	SECTION("Input neurons ont les valeurs attendues")
 	{
-		REQUIRE(red.layers[0][0].getValor() == 1);
-		REQUIRE(red.layers[0][1].getValor() == 3);
+		CHECK(red.layers[0][0].getValor() == 1);
+		CHECK(red.layers[0][1].getValor() == 3);
 	}
 }
 
@@ -193,7 +193,7 @@ TEST_CASE("La fonction getResult renvoie le bon resultat")
 	int structure[] = { 3,2,3,1 };
 	Red red = Red(structure);
 	red.layers[red.nLayers - 1][0].setValor(6);
-	REQUIRE(red.getResult() == 6);
+	CHECK(red.getResult() == 6);
 }
 
 TEST_CASE("La fonction getGenes renvoie la valeur correcte")
