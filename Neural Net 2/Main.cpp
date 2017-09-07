4d7652744b597e2ce2c50d2d23cba2381643f6c9
Raul6469 <24607388+Raul6469@users.noreply.github.com>
2017-09-07 14:33:09 +0200

    Parameters detected
    p to disable print
    s to stop after execution

--- Neural Net 2/Main.cpp
+++ Neural Net 2/Main.cpp
@@ -9,6 +9,45 @@ using namespace std;
 void main(int argc, char* argv[]){
 	srand ((unsigned int) time(NULL));
 
+	bool print = true;
+	bool stop = true;
+	
+	if(argc == 2)
+	{
+		if (argv[1][0] == 'p')
+		{
+			print = false;
+		}
+		if (argv[1][0] == 's')
+		{
+			stop = false;
+		}
+	}
+
+	if (argc == 3)
+	{
+		if (argv[1][0] == 'p' || argv[2][0] == 'p')
+		{
+			print = false;
+		}
+		if (argv[1][0] == 's' || argv[2][0] == 's')
+		{
+			stop = false;
+		}
+	}
+
+	//Remember 'p' as performance, as it disables print statements
+	if ((argc > 1 && argv[1][0] == 'p') || (argc > 2 && (argv[1][0] == 'p' || argv[2][0] == 'p')))
+	{
+		bool print = false;
+		cout << "false" << endl;
+	}
+	else 
+	{
+		bool print = true;
+		cout << "true" << endl;
+	}
+
 	int structure[]={3,2,3,1};
 	double input[]={1,3};
 
@@ -21,7 +60,7 @@ void main(int argc, char* argv[]){
 	red.geneForwardProp();
 	red.forwardProp();
 
-	if (argc > 1 && argv[1][0] != 's' && argv[2][0] != 's')
+	if (stop)
 	{
 		getch();
 	}
