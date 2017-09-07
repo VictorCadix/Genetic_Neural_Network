0d2b2763956d36028ae91a7f295496064b5f5f33
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2017-09-07 14:02:00 +0200

    Files added

---
+++ Neural Net 2/Population.h
@@ -0,0 +1,15 @@
+#ifndef _POPULATION_H 
+#define _POPULATION_H
+#include "Red.h"
+#include <iostream>
+
+using namespace std;
+
+class Population {
+	int population_size;
+	Red *population;
+
+
+};
+
+#endif
