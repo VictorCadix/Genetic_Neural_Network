b423b3ae9db4a18e9decfff3f0b38d44d3435585
Raul6469 <24607388+Raul6469@users.noreply.github.com>
2017-09-08 11:16:21 +0200

    Population generation ok

--- Neural Net 2/Population.cpp
+++ Neural Net 2/Population.cpp
@@ -1,3 +1,18 @@
 #include "stdafx.h"
 
 #include "Population.h"
+
+Population::Population(int *structure, int size)
+{
+	this->population_size = size;
+	this->individus = new Red[size];
+
+	for (int i = 0; i < this->population_size; i++)
+	{
+		Red red = Red(structure);
+		red.setRandomGenes();
+		red.genes2weights();
+
+		this->individus[i] = red;
+	}
+}
