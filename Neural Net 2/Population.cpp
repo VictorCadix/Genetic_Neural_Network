b859b225d2fedb5a69c4b442108a3b8fadb2cc66
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2017-09-09 01:35:05 +0200

    Constructor

--- Neural Net 2/Population.cpp
+++ Neural Net 2/Population.cpp
@@ -1,1 +1,12 @@
 #include "Population.h"
+
+Population::Population(int size, int* structure) {
+	Red red (structure);
+
+	population = new Red[size];
+
+	double input[] = { 1,5 };
+	for (int pop = 0; pop < size; pop++) {
+		population[pop] = Red(structure);
+	}
+}
