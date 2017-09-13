09f772edf1eb2409744e1fcdacac54e464a06616
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2017-09-13 11:40:20 +0200

    Plusieurs données (main)

--- Neural Net 2/Main.cpp
+++ Neural Net 2/Main.cpp
@@ -7,16 +7,19 @@
 
 using namespace std;
 
-void main(int argc, char* argv[]){
-	srand ((unsigned int) time(NULL));
+void main(int argc, char* argv[]) {
+	srand((unsigned int)time(NULL));
 
-	int structure[]={3,2,3,1};
-	double input[][] = { 0.1,0.3 };
-	double expected_result[] = { 0.2 };
+	int structure[] = { 3,2,3,1 };
+	double input[2][2] = { { 0.1,0.3 }, { 0.2, 0.6 } };
+
+	double expected_result[2][1] = { { 0.2 }, { 0.4 } };
 
 	Population population(structure, 10);
-	population.inputs(input);
-	population.evaluate(expected_result);
+	population.inputs(input[0]);
+	population.evaluate(expected_result[0]);
+	population.inputs(input[1]);
+	population.evaluate(expected_result[1]);
 
 	if (argv[1] == NULL)
 	{
