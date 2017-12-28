0d2d926c06284cc6be70a5d1519776569d19e348
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2017-12-28 22:15:27 +0100

    training data file

--- Neural Net 2/Main.cpp
+++ Neural Net 2/Main.cpp
@@ -15,29 +15,45 @@ void main(int argc, char* argv[]) {
 	ofstream training_output;
 	training_output.open("training_output.txt");
 
+	FILE *training_data = fopen("training_data.txt", "r");
+
 	int structure[] = { 3,2,3,1 };
 	
 	//Inputs
 	double** input;
-
-	input = new double *[2];
-	for (int i = 0; i < 2; i++) {
-		input[i] = new double[2];
-	}
-	input[0][0] = 0.1;
-	input[0][1] = 0.3;
-	input[1][0] = 0.2;
-	input[1][1] = 0.6;
-
-	//Outputs
 	double** expected_result;
-	
-	expected_result = new double*[2];
-	for (int i = 0; i < 2; i++) {
-		expected_result[i] = new double[1];
+	int total_data;
+	int nInputs;
+	int nOutputs;
+	int cont = 0;
+
+	while (!feof(training_data)) {
+		char line[50];
+		char token;
+		fgets(line, 50, training_data);
+		sscanf(line, "%c", &token);
+		
+		if (token == '%') {
+			//Its a comment
+		}
+		else if (token == '#') {
+			sscanf(line, "%c %d %*c %d %*c %d", &token, &total_data, &nInputs, &nOutputs);
+			
+			//Allocating memory for the input
+			input = new double *[total_data];
+			for (int i = 0; i < total_data; i++) {
+				input[i] = new double[2];
+			}
+			//Allocating memory for the output
+			expected_result = new double*[total_data];
+			for (int i = 0; i < 2; i++) {
+				expected_result[i] = new double[nOutputs];
+			}
+		}
+		else {
+			sscanf(line, "%lf %lf %*c %lf", &input[cont][0], &input[cont][1], &expected_result[cont][0]);
+		}
 	}
-	expected_result[0][0] = 0.2;
-	expected_result[1][0] = 0.4;
 	
 
 	Population population(structure, 10);
